2da5b48914ea42e5c403bef1f345414a811e0651
Matt <contact@mattjbrady.com>
2021-10-30 19:11:05 +0000

    Added comments to intial tests

--- spec/controller/project_spec.rb
+++ spec/controller/project_spec.rb
@@ -1,18 +1,25 @@
 require "rails_helper"
 
 RSpec.describe ProjectsController, type: :controller do
+    # Test index 
     context "GET #index" do
+        # Test that controller can route to index
         it "returns a success response" do
             get :index
             # expect(response.success).to eq(true)
+            # Test passes if route returns a success
             expect(response).to be_success
         end
     end
 
+    # Test individual projects
     context "GET #show" do
+        # Create a test project
         let!(:project) { Project.create(title: "Test title", description: "Test description") }
+        # Test that controller can route to test project
         it "returns a success response" do
             get :show, params: { id: project }
+            # Test passes if route returns a success
             expect(response).to be_success
         end
     end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Project, type: :model do
     context "validations tests" do
         it "ensures the title is present" do
             project = Project.new(description: "Content of the description")
+            # Test should fail if projects can be created without title
             expect(project.valid?).to eq(false)
         end
 
@@ -13,13 +14,16 @@ RSpec.describe Project, type: :model do
     end
 
     context "scopes tests" do
+        # Create a set of parameters
         let(:params) { { title: "Title", description: "some description" } }
+        # Create three projects
         before(:each) do
             Project.create(params)
             Project.create(params)
             Project.create(params)
         end
-
+        
+        # All three projects should exist
         it "should return all projects" do
             expect(Project.count).to eq(3)
         end
