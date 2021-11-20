544f745a3f269f8c33273c1dde45c9876c79f4cd
Matt <contact@mattjbrady.com>
2021-11-20 23:41:11 +0000

    Added model unit tests for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation tests" do
+    it "ensures user has a password" do
+      # Create user without password value
+      user = User.new(email: "testuser@testemail.com")
+
+      # Should not be able to create a user without a password
+      expect(user.valid?).to eq(false)
+    end
+  end
+
+  # Test that the multiple users can exist
+  context "scopes tests" do
+    # Create three users
+    before(:each) do
+        User.create(email: "testuser1@testemail.com", password: "abcdef")
+        User.create(email: "testuser2@testemail.com", password: "ghijkl")
+        User.create(email: "testuser3@testemail.com", password: "mnopqr")
+    end
+    
+    # All three projects should exist
+    it "should return all users" do
+        expect(User.count).to eq(3)
+    end
+end
 end
